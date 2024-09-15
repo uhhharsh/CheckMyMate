709936f5a975977db0e3191fab474a5d2790c768
uhhharsh <harshsaini5085@gmail.com>
2024-09-15 06:15:57 +0530

    shadcn installation

---
+++ src/app/model/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const model = () => {
+  return  ( 
+    <>
+        <img src="/Users/harshsaini/Desktop/btechProject/website/frontend/public/assets/mahdis-mousavi-hJ5uMIRNg5k-unsplash.jpg" alt="ML" />
+        </>
+  )
+}
+
+export default model;
